1f727bdd94d12e81b3ca2b7e98a8bbd1473dc062
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-12-20 17:32:26 +0100

    Javadoc

--- src/main/java/io/github/oliviercailloux/git/common/RepositoryCoordinates.java
+++ src/main/java/io/github/oliviercailloux/git/common/RepositoryCoordinates.java
@@ -3,11 +3,14 @@ package io.github.oliviercailloux.git.common;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.net.InternetDomainName;
+
 /**
  * The coordinates of a repository constituted of an authority, an owner and a repository name and
  * reachable at the git uri {@code ssh://authority/owner/repo.git}. This format is used by such
  * hosts as <a href="https://github.com/">GitHub</a> or
  * <a href="https://bitbucket.org/">BitBucket</a>.
+ * <p>This class also accepts authorities of the form {@code host:port} or using an IP address instead of a domain name.
  */
 public class RepositoryCoordinates {
   public static RepositoryCoordinates from(String authority, String owner, String repo) {
@@ -40,6 +43,10 @@ public class RepositoryCoordinates {
     return repo;
   }
 
+  /**
+   * @deprecated This only identifies the entry given an authority, so I doubt that valid use cases exist for this concept.
+   */
+  @Deprecated
   public String id() {
     return owner + "/" + repo;
   }
