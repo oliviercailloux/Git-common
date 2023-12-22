a83c0278635af8da4cfc4558a355912c2845076d
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-12-22 20:05:45 +0100

    tests (from jfs)

---
+++ src/test/java/io/github/oliviercailloux/git/RepositoryCoordinatesTests.java
@@ -0,0 +1,16 @@
+package io.github.oliviercailloux.git;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.github.oliviercailloux.git.common.GitUri;
+import io.github.oliviercailloux.git.common.RepositoryCoordinates;
+import org.junit.jupiter.api.Test;
+
+public class RepositoryCoordinatesTests {
+  @Test
+  void testUri() throws Exception {
+    final GitUri obtained = RepositoryCoordinates
+        .from("git@github.com", "oliviercailloux-org", "minimax-ex").asGitUri();
+    assertEquals("ssh://git@github.com/oliviercailloux-org/minimax-ex.git", obtained.asString());
+  }
+}
